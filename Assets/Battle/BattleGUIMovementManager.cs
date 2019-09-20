0a0e1c446caac4a1e1a6ab32623cba7e0560b265
lacuna.andru.maggert <lacuna.andru.maggert@gmail.com>
2019-09-20 16:05:35 -0500

    add stub battleGuiMovementManager

---
+++ Assets/Battle/BattleGUIMovementManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleGUIMovementManager : MonoBehaviour
+{
+    public GameObject GUIBattleSelector;
+
+    public InputReader InputReader;
+
+
+    //track input state here:
+    //state one: select character
+    //state two: selection GUI
+
+        //function enable GUIBattleSelector and pass in the referencial character?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
